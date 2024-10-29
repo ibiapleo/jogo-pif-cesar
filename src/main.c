3e388ac0891e488a284bea3faa873876cfd94e5d
eniobazante <eniom.bazante@gmail.com>
2024-10-29 02:08:40 +0000

    corrigindo os headers

--- src/main.c
+++ src/main.c
@@ -1,88 +1,37 @@
-/**
- * main.h
- * Created on Aug, 23th 2023
- * Author: Tiago Barros
- * Based on "From C to C++ course - 2002"
-*/
-
 #include <string.h>
-
 #include "screen.h"
 #include "keyboard.h"
 #include "timer.h"
 
-int x = 34, y = 12;
-int incX = 1, incY = 1;
-
-void printHello(int nextX, int nextY)
-{
-    screenSetColor(CYAN, DARKGRAY);
-    screenGotoxy(x, y);
-    printf("           ");
-    x = nextX;
-    y = nextY;
-    screenGotoxy(x, y);
-    printf("Hello World");
-}
-
-void printKey(int ch)
-{
-    screenSetColor(YELLOW, DARKGRAY);
-    screenGotoxy(35, 22);
-    printf("Key code :");
-
-    screenGotoxy(34, 23);
-    printf("            ");
-    
-    if (ch == 27) screenGotoxy(36, 23);
-    else screenGotoxy(39, 23);
-
-    printf("%d ", ch);
-    while (keyhit())
-    {
-        printf("%d ", readch());
-    }
-}
-
-int main() 
-{
+// Inicialização da biblioteca na main
+int main() {
     static int ch = 0;
-
+    // Desenha as bordas da tela do jogo d
     screenInit(1);
+    // Inicializa a captura de dados do teclado
     keyboardInit();
+    // Define o atraso dos frames no jogo
     timerInit(50);
-
-    printHello(x, y);
+    // Inicializa atualizando a tela
     screenUpdate();
-
-    while (ch != 10) //enter
-    {
-        // Handle user input
-        if (keyhit()) 
-        {
-            ch = readch();
-            printKey(ch);
-            screenUpdate();
-        }
-
-        // Update game state (move elements, verify collision, etc)
-        if (timerTimeOver() == 1)
-        {
-            int newX = x + incX;
-            if (newX >= (MAXX -strlen("Hello World") -1) || newX <= MINX+1) incX = -incX;
-            int newY = y + incY;
-            if (newY >= MAXY-1 || newY <= MINY+1) incY = -incY;
-
-            printKey(ch);
-            printHello(newX, newY);
-
-            screenUpdate();
+    // O loop do jogo segue até pressionar enter (10)
+    while (ch != 10) {
+    // Verifica se uma tecla foi pressionada
+    if (keyhit()) {
+    // Captura o caractere pressionado
+    ch = readch();
+    // Atualiza a tela
+    screenUpdate();
+    }
+    // Atualiza o estado do jogo (mover elementos, verificar colisões, etc)
+    if (timerTimeOver() == 1) {
+    // Atualiza a tela
+    screenUpdate();
         }
     }
-
+    // Desaloca os recursos utilizados pela cli-lib
     keyboardDestroy();
     screenDestroy();
     timerDestroy();
-
     return 0;
 }
