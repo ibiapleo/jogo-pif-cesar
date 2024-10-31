6c77d906802d1f8568d50954e2c362cb98869eb0
GabrielMorais2 <moraisgabriel274@gmail.com>
2024-10-31 19:26:03 -0300

    feat: bullet and wing

---
+++ include/bullet.h
@@ -0,0 +1,21 @@
+/**
+ * WING.h
+ * Created on Oct, 29th 2024
+ * Author: Leonardo Ibiapina
+*/
+
+#ifndef __BULLET_H__
+#define __BULLET_H__
+
+void printBullet();
+
+void clearBullet();
+
+void shootBullets();
+
+void moveBullets();
+
+void moveWingBullet();
+
+
+#endif /* __BULLET_H__ */
